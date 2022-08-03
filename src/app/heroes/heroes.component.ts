dc6e167f622725750d29dc5cc67a52108358f2ec
lucasfv <lucas@correlation-one.com>
2022-08-02 23:42:04 -0300

    Add click event binding
    This commit adds a click event to the list of heroes.
    This way we can interact with the page and act when the user clicks
    a button from the list.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,10 +12,11 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Windstorm',
-  // };
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   heroes = HEROES;
 }
