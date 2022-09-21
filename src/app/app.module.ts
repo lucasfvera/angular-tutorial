56edbfc2111c5af568bd6f8aecebe8eab9553015
lucasfvera <lucasfvera@hotmail.com>
2022-09-21 02:33:15 -0300

    Enable HTTP service and mock data server
    We use the HttpClient module from Angular by including it in the app
    module. This would be useful to comunicate with the server.
    
    To avoid creating the server yet, we simulate the responses and the
    DB with the in-memory Web API library. We install it with
    `npm install angular-in-memory-web-api --save` and generate the
    proper service with `ng generate service ...`.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -17,8 +20,21 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     MessagesComponent,
     DashboardComponent,
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+  dataEncapsulation: false,
+});
